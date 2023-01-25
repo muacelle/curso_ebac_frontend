16bbd89d3594bb52ea8202080f6230c6b32e6215
Marcelle Pernet <mevpernet@gmail.com>
2023-01-25 16:10:53 -0300

    first commit

--- main.js
+++ main.js
@@ -1,33 +1,14 @@
-const form = document.getElementById("form-valores");
+$(document).ready(function() {
+    // Adicionar nova tarefa
+    $('#add-button').click(() => {
+        let task = $('#task-input').val();
 
-function validarValores(a, b) {
-    return parseInt(b) > parseInt(a);
-}
+        $('#task-list').append('<li>' + task + '</li>');
+        $('#task-input').val('');
+    });
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const valorA = document.getElementById("valor-a");
-    const valorB = document.getElementById("valor-b");
-
-    const mensagemSucesso = `Formulário válido! <b>${valorB.value}</b> é maior que <b>${valorA.value}</b>.`;
-    const mensagemErro = `Formulário inválido! <b>${valorB.value}</b> NÃO é maior que <b>${valorA.value}</b>.`
-
-    let formValido = false;
-    formValido = validarValores(valorA.value, valorB.value);
-
-    if (formValido) {
-        
-        document.getElementById("mensagem-sucesso").innerHTML = mensagemSucesso;
-        document.getElementById("mensagem-sucesso").style.display = "block";
-        document.getElementById("mensagem-erro").style.display = "none";
-
-        valorA.value = "";
-        valorB.value = "";
-
-    } else {
-        document.getElementById("mensagem-erro").innerHTML = mensagemErro;
-        document.getElementById("mensagem-erro").style.display = "block";
-        document.getElementById("mensagem-sucesso").style.display = "none";
-    }
-})
+    // Riscar uma tarefa
+    $('#task-list').on('click', 'li', (event) => {
+        $(event.target).css('text-decoration', 'line-through');
+    });
+});
