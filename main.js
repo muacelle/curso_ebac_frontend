777b8a0771a3c89c498716f36dc8f184cd147157
Marcelle Pernet <mevpernet@gmail.com>
2023-01-25 19:49:21 -0300

    first commit

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-$(document).ready(function() {
-    // Adicionar nova tarefa
-    $('#add-button').click(() => {
-        let task = $('#task-input').val();
+$(document).ready(() => {
 
-        $('#task-list').append('<li>' + task + '</li>');
-        $('#task-input').val('');
-    });
+    // Masks
+    $("#number").mask("(00) 00000-0000", {placeholder: "(11) 91234-1234"});
+    $("#cpf").mask("000.000.000-00", {placeholder: "987.654.321-00"}, {reverse: true});
+    $("#cep").mask("00000-000", {placeholder: "00000-000"});
 
-    // Riscar uma tarefa
-    $('#task-list').on('click', 'li', (event) => {
-        $(event.target).css('text-decoration', 'line-through');
-    });
-});
+    // Validação
+    $("#form").validate({
+        rules: {
+            fullname: { required: true },
+            email: { required: true, email: true },
+            number: { required: true, minlength: 11 },
+            cpf: { required: true, minlength: 11 },
+            cep: { required: true, minlength: 8 },
+            address: { required: true },
+        }, 
+    })
+
+})
